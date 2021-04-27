3828348e256332083ff3503798613e16922e8a2d
Brotiger <dimka@bdima.ru>
2021-04-27 21:13:09 +0400

    paginator

--- resources/views/employeesList.blade.php
+++ resources/views/employeesList.blade.php
@@ -1,91 +1,103 @@
 @extends('layouts.main')
-@section('title', 'Сотрудники (ПГУТИ)')
+@section('title', 'Главная')
 @section('content')
     <div class="container-fluid">
-        <div class="row tools">
-            <div class="col text-center"><a href="javascript:history.go(-1)"><i class="fas fa-user-plus"></i></a></div>
-            <div class="col-10 text-center">Список сотрудников {{ $name }}</div>
-            <div class="col text-center"><a href="javascript:history.go(+1)"><i class="fas fa-search"></a></i></div>
-        </div>
-        <div class="dbList">
+        <div class="mt-5 dbList">
             @if($employees->count() > 0)
-                @foreach($employees as $employee)
-                    <div employee-id="{{ $employee->id }}" class="emp">
-                        <div>
-                            <img src="{{ $employee->img? 'http://127.0.0.1:8000/storage/'. $employee->img : '/images/no-profile.png' }}">
-                        </div>
-                        <div class="info">
-                            <div><b>Фамилия:</b> {{ $employee->lastName }}</div>
-                            <div><b>Имя:</b> {{ $employee->firstName }}</div>
-                            <div><b>Отчество:</b> {{ $employee->secondName }}</div>
-                            <div><b>Дата рождения:</b> {{ !empty($employee->dateBirthday)? date('m-d-Y', strtotime($employee->dateBirthday)) : 'Неизвестно' }}</div>
-                            <div><b>Дата приема:</b> {{ !empty($employee->hired)? date('m-d-Y', strtotime($employee->hired)) : 'Неизвестно' }}</div>
-                            <div><b>Дата увольнения:</b> {{ !empty($employee->fired)? date('m-d-Y', strtotime($employee->fired)) : 'Неизвестно' }}</div>
-                        </div>
-                    </div>
-                @endforeach
+            <div class="mb-1 text-center">
+                <h1>Список сотрудников {{ $name }}</h1>
+                <span>Для того что бы просмотреть подробную информацию о сотруднике нажмите на него</span>
+            </div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Фамилия</th>
+                        <th>Имя</th>
+                        <th>Отчество</th>
+                        <th colspan="4">Дата рождения</th>
+                    </tr>
+                    <tr>
+                        <th><input type="text" class="form-control" placeholder="Фамилия" filter-field id="lastName" autocomplete="off"></th>
+                        <th><input type="text" class="form-control" placeholder="Имя" filter-field id="firstName" autocomplete="off"></th>
+                        <th><input type="text" class="form-control" placeholder="Отчество" filter-field id="secondName" autocomplete="off"></th>
+                        <th><input type="date" class="form-control mb-1" placeholder="С:" filter-field id="dateBirthdayFrom"></th><th><input type="date" class="form-control" placeholder="По:" filter-field id="dateBirthdayTo"></th>
+                        <th><button class="form-control btn btn-danger mb-1" id="reset"><i class="bi bi-arrow-counterclockwise"></i></button></th><th><button class="form-control btn btn-primary" id="search"><i class="bi bi-search"></i></button></th>
+                    </tr>
+                </thead>
+                <tbody id="employeesTable">
+                    @foreach($employees as $employee)
+                        <tr class="recordRow" employee-id="{{ $employee->id }}">
+                            <td>{{ $employee->lastName }}</td>
+                            <td>{{ $employee->firstName }}</td>
+                            <td>{{ $employee->secondName }}</td>
+                            <td colspan="4">{{ !empty($employee->dateBirthday)? date('m-d-Y', strtotime($employee->dateBirthday)) : '' }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
             @else
             <p>
                 Ничего не найдено
             </p>
             @endif
         </div>
+        {{ $employees->links('vendor.pagination.bootstrap-4') }}
     </div>
 @endsection
 @section('custom_js')
-<script>
-    $(document).ready(function(){
-        $('#employeesTable').delegate('.recordRow', 'click', function(){
-            let empId = $(this).attr('employee-id')
-            window.location.href = 'edit_employee' + '/' + empId;
-        });
-
-        $('#search').on('click', function(){
-            startLoading();
-            var formData = {};
-
-            $('[filter-field]').each(function(i, ell){
-                formData[ell.id] = $(this).val();
+    <script>
+        $(document).ready(function(){
+            $('#employeesTable').delegate('.recordRow', 'click', function(){
+                let empId = $(this).attr('employee-id')
+                window.location.href = 'edit_employee' + '/' + empId;
             });
 
-            let res = $.ajax({
-                url: "{{route('employees_list')}}",
-                type: "GET",
-                processData: true,
-                contentType: false,
-                data: formData,
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                success: function(data){
-                    stopLoading();
-                    let positionParameters = location.pathname.indexOf('?');
-                    let url = location.pathname.substring(positionParameters, location.pathname.length);
-                    let newUrl = url + '?';
-                    for(key in formData){
-                        if(formData[key]){
-                            newUrl += key + "=" + formData[key] + "&";
+            $('#search').on('click', function(){
+                startLoading();
+                var formData = {};
+
+                $('[filter-field]').each(function(i, ell){
+                    formData[ell.id] = $(this).val();
+                });
+
+                let res = $.ajax({
+                    url: "{{route('employees_list')}}",
+                    type: "GET",
+                    processData: true,
+                    contentType: false,
+                    data: formData,
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function(data){
+                        stopLoading();
+                        let positionParameters = location.pathname.indexOf('?');
+                        let url = location.pathname.substring(positionParameters, location.pathname.length);
+                        let newUrl = url + '?';
+                        for(key in formData){
+                            if(formData[key]){
+                                newUrl += key + "=" + formData[key] + "&";
+                            }
                         }
+                        newUrl = newUrl.slice(0, -1);
+                        //newUrl += "page={{ isset($_GET['page']) ? $_GET['page'] : 1 }}";
+                        history.pushState({}, '', newUrl);
+
+                        $('#employeesTable').html(data)
+                    },
+                    error: function(data){
+                        stopLoading();
+                        $('#error-message').fadeIn(300).delay(2000).fadeOut(300);
                     }
-                    newUrl = newUrl.slice(0, -1);
-                    //newUrl += "page={{ isset($_GET['page']) ? $_GET['page'] : 1 }}";
-                    history.pushState({}, '', newUrl);
+                });
+            });
 
-                    $('#employeesTable').html(data)
-                },
-                error: function(data){
-                    stopLoading();
-                    $('#error-message').fadeIn(300).delay(2000).fadeOut(300);
-                }
+            $('#reset').on('click', function(){
+                $("[filter-field]").each(function(){
+                    $(this).val("");
+                });
+                $('#search').click();
             });
         });
-
-        $('#reset').on('click', function(){
-            $("[filter-field]").each(function(){
-                $(this).val("");
-            });
-            $('#search').click();
-        });
-    });
-</script>
+    </script>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,12 +7,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="css/app.css">
     <link rel="shortcut icon" href="/images/favicon-min.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
 </head>
     <body>
-        <header>
+        <div class="preloader">
+            <svg class="preloader__image" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor"
+                    d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z">
+                </path>
+            </svg>
+        </div>
+        <header class="container-fluid">
             <div class="row">
                 <div class="col text-center"><a href="javascript:history.go(-1)"><i class="fas fa-arrow-left"></i></a></div>
-                <div class="col-10 text-center">Электронный музей ПГУТИ и КС ПГУТИ</div>
+                <div class="col-10 text-center"><a href="/">Электронный музей ПГУТИ и КС ПГУТИ</a></div>
                 <div class="col text-center"><a href="javascript:history.go(+1)"><i class="fas fa-arrow-right"></a></i></div>
             </div>
         </header>
@@ -21,6 +29,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/4f55501494.js" crossorigin="anonymous"></script>
-    <script src="js/app.js"></script>
+    <script src="js/app.js" defer></script>
+    <script src="js/loading.js" defer></script>
+    <script>
+        $(document).ready(function(){
+            stopLoading();
+        });
+    </script>
     @yield('custom_js')
 </html>
